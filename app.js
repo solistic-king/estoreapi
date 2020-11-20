263454bf6b063c8a0a2d00638f15aa778f50802f
Varun Madhu <varunmadhu6@gmail.com>
2020-11-20 19:26:22 +0530

    dd

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.use((error, req, res, next) => {
     res.status(statusCode).json({ message: message, data: data });
 });
 
-const MONGODB_URI = `mongodb+srv://${'varunmadhu6%40gmail.com'}:varun1234@testcluster.n5fgm.mongodb.net/estore`
+const MONGODB_URI = `mongodb+srv://varunmadhu6@gmail.com:varun1234@testcluster.n5fgm.mongodb.net/estore`
 
-mongoose.connect(MONGODB_URI).then(result => {
+mongoose.connect(MONGODB_URI,{ useNewUrlParser: true}).then(result => {
     app.listen(process.env.PORT || 3000);
 }).catch(err => {
     console.log(err);
